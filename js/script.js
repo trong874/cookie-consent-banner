505f0d4410fb9c91a37559ad07aec788a3e09ca2
Phạm Văn Trọng <pvt874@gmail.com>
2023-09-23 23:43:11 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cookieBanner = document.getElementById('cookie-banner');
+    const acceptCookieButton = document.getElementById('accept-cookie');
+
+    // Kiểm tra xem cookie đã được chấp thuận trước đó
+    if (localStorage.getItem('cookieAccepted')) {
+        cookieBanner.style.display = 'none';
+    }
+
+    // Sự kiện khi người dùng chấp nhận cookie
+    acceptCookieButton.addEventListener('click', function() {
+        cookieBanner.style.display = 'none';
+        localStorage.setItem('cookieAccepted', 'true');
+    });
+});
+
+document.addEventListener('alpine:init', () => {
+    Alpine.store('consentStyle', 'consent-tooltip');
+    Alpine.store('position', 'bottom-left');
+    Alpine.store('mainMessage', 'This website uses cookies to ensure you get the best experience on our website.');
+    Alpine.store('linkText', 'Learn More');
+    Alpine.store('urlCookiePolicy', 'https://www.yoursite.com/cookie-policy/');
+    Alpine.store('buttonText', 'Got it!');
+})
